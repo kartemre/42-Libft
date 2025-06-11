24ecc2a6c702db24b3ed1259ff52f3b09480b047
Emre Kart <ekart@k1m42s01.42istanbul.com.tr>
2025-06-11 22:42:10 +0300

    updated: libft.h

---
+++ libft.h
@@ -0,0 +1,13 @@
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int ft_isalpha(int);
+int ft_isdigit(int);
+int ft_isalnum(int);
+int ft_isascii(int);
+int ft_isprint(int);
+
+#endif
