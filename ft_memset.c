62c29264e7d5e8e86d0d72343e2625fc34711b67
Emre Kart <ekart@k1m42s01.42istanbul.com.tr>
2025-06-11 22:43:05 +0300

    feat: implement ft_memset to fill memory with a constant byte

---
+++ ft_memset.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ekart <ekartk@student.42istanbul.com.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/11 20:11:27 by ekart             #+#    #+#             */
+/*   Updated: 2025/06/11 20:20:40 by ekart            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*dest;
+
+	dest = (unsigned char *)s;
+	while (n--)
+		*dest++ = (unsigned char)c;
+	return (s);
+}
