c3741a82c225c8f5cbdaf28a27a2c68659f5ba95
Emre Kart <ekart@k1m17s06.42istanbul.com.tr>
2025-06-16 18:03:45 +0300

    feat: implement ft_memchr to search for a byte in memory

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,8 +6,23 @@
 /*   By: ekart <ekartk@student.42istanbul.com.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 19:24:49 by ekart             #+#    #+#             */
-/*   Updated: 2025/06/15 19:24:51 by ekart            ###   ########.fr       */
+/*   Updated: 2025/06/16 12:50:30 by ekart            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char *ptr = (unsigned char)s;
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (ptr[i] == (unsigned char)c)
+			return ((void *)(ptr + i));
+		i++;
+	}
+	return (NULL);
+}
