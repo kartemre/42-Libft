b7d6e3a3c422511e9be652864dcfea111fc61640
Emre Kart <ekart@k1m43s02.42istanbul.com.tr>
2025-06-17 20:10:33 +0300

    feat: implement ft_calloc to allocate and zero-initialize memory

---
+++ ft_calloc.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ekart <ekartk@student.42istanbul.com.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/17 13:41:23 by ekart             #+#    #+#             */
+/*   Updated: 2025/06/17 13:41:26 by ekart            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+	size_t	ttl;
+
+	ttl = nmemb * size;
+	if (nmemb != 0 && ttl / nmemb != size)
+		return (NULL);
+
+	ptr = malloc(ttl);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, ttl);
+	return (ptr);
+}
